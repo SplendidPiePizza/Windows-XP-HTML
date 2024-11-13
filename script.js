b8c548595bb5f854d123eddb36c9df3d72a2766d
SplendidPiePizza <165284283+SplendidPiePizza@users.noreply.github.com>
2024-11-13 19:03:26 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,37 @@
-// Get modal and close button
+// Get elements
+var startBtn = document.getElementById("start-btn");
+var startMenu = document.getElementById("start-menu");
 var modal = document.getElementById("modal");
 var closeModal = document.getElementById("close-modal");
 
-// Open modal when "My Computer" icon is clicked
+// Toggle start menu visibility
+startBtn.addEventListener("click", function() {
+    // Toggle the display of the start menu
+    if (startMenu.style.display === "block") {
+        startMenu.style.display = "none";
+    } else {
+        startMenu.style.display = "block";
+    }
+});
+
+// Close the start menu if clicked outside
+window.addEventListener("click", function(event) {
+    if (!startBtn.contains(event.target) && !startMenu.contains(event.target)) {
+        startMenu.style.display = "none";
+    }
+});
+
+// Open modal for "My Computer"
 document.getElementById("my-computer").addEventListener("click", function() {
     modal.style.display = "flex";
 });
 
-// Close modal when close button is clicked
+// Close modal
 closeModal.addEventListener("click", function() {
     modal.style.display = "none";
 });
 
-// Close modal if the user clicks outside the modal content
+// Close modal if clicked outside
 window.addEventListener("click", function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
